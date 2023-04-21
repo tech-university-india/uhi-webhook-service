065f2b79621abd2907f8e3f1ae9bd83a6ea44312
Kushagraved <kushagraved@gmail.com>
2023-04-21 11:06:30 +0530

    chore: fix kafka

--- src/utils/kafka/producer.js
+++ src/utils/kafka/producer.js
@@ -1,79 +1,31 @@
-// const { Kafka } = require('kafkajs')
-// require('dotenv').config()
-// const kafka = new Kafka({
-//   clientId: process.env.KAFKA_PRODUCER_CLIENT_ID,
-//   brokers: [process.env.KAFKA_BROKERS]
-// })
+const { Kafka } = require('kafkajs')
+const { allTopics } = require('../../config/config')
+require('dotenv').config()
 
-const { KafkaClient, Producer } = require('kafka-node')
+const kafka = new Kafka({
+  clientId: process.env.KAFKA_PRODUCER_CLIENT_ID,
+  brokers: [process.env.KAFKA_BROKERS]
+})
 
-const client = new KafkaClient({ kafkaHost: process.env.KAFKA_BROKERS })
-const producer = new Producer(client)
+const producer = kafka.producer()
+const admin = kafka.admin()
 const initProducer = async () => {
-  client.connect()
-  client.on('ready', () => {
-    console.log('Kafka Client is ready')
-    client.createTopics(['auth', 'careContext'], (err, res) => {
-      if (err) {
-        throw err
-      }
-      console.log('TOPIC CREATED')
+  await admin.connect()
+  const topics = await admin.listTopics()
+  const topicsToCreate = allTopics.filter((topic) => !topics.includes(topic))
+  if (topicsToCreate.length > 0) {
+    await admin.createTopics({
+      topics: topicsToCreate.map((topic) => ({ topic }))
     })
-    // client.topicExists('auth', (err, res) => {
-    //   if (err) {
-    //     throw err
-    //   }
-    //   if (!res) {
-
-    //   }
-    // })
-  })
+  }
+  await producer.connect()
 }
 
 const produceMessage = async (topic, message) => {
-  const payloads = [
-    {
-      topic,
-      messages: [JSON.stringify(message)]
-    }
-  ]
-  producer.send(payloads, (err, data) => {
-    if (err) {
-      throw err
-    }
-    console.log('data', data)
+  await producer.send({
+    topic,
+    messages: [{ value: JSON.stringify(message) }]
   })
 }
-
-// const producer = kafka.producer()
-// const admin = kafka.admin()
-
-// const initProducer = async () => {
-//   await admin.connect()
-//   const topics = await admin.listTopics()
-//   console.log('topics', topics)
-
-//   if (!topics.includes('auth')) {
-//     await admin.createTopics({
-//       waitForLeaders: true,
-//       topics: [
-//         {
-//           topic: 'auth'
-//         }
-//       ]
-//     })
-//     const topics = await admin.listTopics()
-//     console.log('topics', topics)
-//   }
-//   await producer.connect()
-// }
-
-// const produceMessage = async (topic, message) => {
-//   console.log('produceMessage', topic, message)
-//   await producer.send({
-//     topic,
-//     messages: [{ value: JSON.stringify(message) }]
-//   })
-// }
 
 module.exports = { initProducer, produceMessage }
