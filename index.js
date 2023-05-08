12c3f0fb5e4ab90c6a55e958f51b590d17672d08
Kartik Goel <59787066+kartik162000@users.noreply.github.com>
2023-05-08 17:40:48 +0530

    fix: update linting configs and setup prettier (#12)

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const { initProducer } = require('./src/utils/kafka/producer')
-const { webhook } = require('./src/controllers/webhook')
-const { tokenValidator } = require('./src/middleware/tokenValidate')
-const logger = require('./src/utils/logging')
-app.use(express.json())
-app.use(express.text())
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const {initProducer} = require('./src/utils/kafka/producer');
+const {webhook} = require('./src/controllers/webhook');
+const {tokenValidator} = require('./src/middleware/tokenValidate');
+const logger = require('./src/utils/logging');
+app.use(express.json());
+app.use(express.text());
 
 app.use(
   cors({
-    origin: '*'
+    origin: '*',
   })
-)
+);
 
 app.get('/latest/:id', (req, res) => {
-  const id = req.params.id
+  const id = req.params.id;
   if (id === 'server') {
-    res.sendFile('logs/server.log', { root: __dirname })
+    res.sendFile('logs/server.log', {root: __dirname});
   } else {
-    res.sendFile('logs/kafka.log', { root: __dirname })
+    res.sendFile('logs/kafka.log', {root: __dirname});
   }
-})
+});
 
-app.use('*', tokenValidator, webhook)
-initProducer().catch((error) => {
-  console.log('Error in connecting to Kafka', error)
-  logger.kafkaLogger.error(`Error in connecting to Kafka ${error}`)
-})
+app.use('*', tokenValidator, webhook);
+initProducer().catch(error => {
+  console.log('Error in connecting to Kafka', error);
+  logger.kafkaLogger.error(`Error in connecting to Kafka ${error}`);
+});
 
-const PORT = process.env.PORT || 9007
+const PORT = process.env.PORT || 9007;
 
-app.listen(PORT, () => console.log(`WebHook Service Started on port ${PORT}`))
+app.listen(PORT, () => console.log(`WebHook Service Started on port ${PORT}`));
--- tests/controllers/webhook.test.js
+++ tests/controllers/webhook.test.js
@@ -1,37 +1,39 @@
-const { webhook } = require('../../src/controllers/webhook')
-const requestHandler = require('../../src/services/requestHandler')
+const {webhook} = require('../../src/controllers/webhook');
+const requestHandler = require('../../src/services/requestHandler');
 
 describe('Webhook Controller', () => {
   it('should return 200 and OK when request recieved', async () => {
-    jest.spyOn(requestHandler, 'request').mockResolvedValue('Ok')
+    jest.spyOn(requestHandler, 'request').mockResolvedValue('Ok');
     const req = {
       headers: {},
       query: {},
       body: {},
-      baseUrl: ''
-    }
+      baseUrl: '',
+    };
     const res = {
       status: jest.fn().mockReturnThis(),
-      send: jest.fn()
-    }
-    await webhook(req, res)
-    expect(res.status).toHaveBeenCalledWith(200)
-    expect(res.send).toHaveBeenCalledWith('OK')
-  })
+      send: jest.fn(),
+    };
+    await webhook(req, res);
+    expect(res.status).toHaveBeenCalledWith(200);
+    expect(res.send).toHaveBeenCalledWith('OK');
+  });
   it('should return 400 and error message when request recieved', async () => {
-    jest.spyOn(requestHandler, 'request').mockRejectedValue(new Error('Invalid Path'))
+    jest
+      .spyOn(requestHandler, 'request')
+      .mockRejectedValue(new Error('Invalid Path'));
     const req = {
       headers: {},
       query: {},
       body: {},
-      baseUrl: ''
-    }
+      baseUrl: '',
+    };
     const res = {
       status: jest.fn().mockReturnThis(),
-      send: jest.fn()
-    }
-    await webhook(req, res)
-    expect(res.status).toHaveBeenCalledWith(400)
-    expect(res.send).toHaveBeenCalledWith('Invalid Path')
-  })
-})
+      send: jest.fn(),
+    };
+    await webhook(req, res);
+    expect(res.status).toHaveBeenCalledWith(400);
+    expect(res.send).toHaveBeenCalledWith('Invalid Path');
+  });
+});
